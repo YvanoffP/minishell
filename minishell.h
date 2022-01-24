29e139f6e7ddd9803c5a11b56295ff81cc2c83f9
YvanoffP <petruzzi.yvan@gmail.com>
2022-01-24 14:52:27 +0100

    header file created

---
+++ minishell.h
@@ -0,0 +1,12 @@
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include <unistd.h>
+# include <stdio.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+# include <sys/stat.h>
+# include <sys/wait.h>
+# include <termios.h>
+
+#endif
