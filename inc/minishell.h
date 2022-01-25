76d8a89d349161a7319035129b4e219550a4171f
YvanoffP <petruzzi.yvan@gmail.com>
2022-01-25 16:22:23 +0100

    preparing the main structure

---
+++ inc/minishell.h
@@ -0,0 +1,15 @@
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include <unistd.h>
+# include <string.h>
+# include <stdio.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+# include <sys/stat.h>
+# include <sys/types.h>
+# include <sys/wait.h>
+# include <termios.h>
+# include <dirent.h>
+
+#endif
