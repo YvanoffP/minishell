e8db82136f9fd38b017b123acab7bb04012afcf7
YvanoffP <petruzzi.yvan@gmail.com>
2022-02-11 18:55:49 +0100

    still an issue with case echo  DOUBLE QUOtED  et  ont un fils

--- src/parse/parsing.c
+++ src/parse/parsing.c
@@ -131,9 +131,107 @@ int	*catch_quote(char const *s, unsigned int start, int len)
 	return (quotes);
 }
 
-char	*cpy_str(char const *s, int start, int len)
+int	have_a_dollar(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] == '$')
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+char *find_env_var(char *str, int *i, t_env **env_list)
+{
+	char *name;
+	int	j;
+	t_env	*list;
+
+	*i += 1;
+	j = *i;
+	list = *env_list;
+	while (!is_w_space(str[*i]) && str[*i])
+		*i += 1;
+	name = ft_substr(str, j, (*i - j));
+	while (ft_strcmp(name, list->var) && list->next != NULL)
+		list = list->next;
+	if (ft_strcmp(name, list->var))
+	{
+		free(name);
+		return (NULL);
+	}
+	free(name);
+	name = ft_strdup(list->value);
+	*i -= 1;
+	return (name);
+}
+
+char *append(char *ret, char *tmp, char *value)
+{
+	char *old_ret;
+	char *new_ret;
+
+	if (!value && !ret)
+		return (ft_strdup(tmp));
+	else if (!value && ret)
+		return (ft_strjoin(ret, tmp));
+	else if (value && !ret)
+		return (ft_strjoin(tmp, value));
+	else
+	{
+		old_ret = ft_strdup(ret);
+		free(ret);
+		ret = ft_strjoin(tmp, value);
+		new_ret = ft_strjoin(old_ret, ret);
+		free(old_ret);
+		free(ret);
+	}
+	return (new_ret);
+}
+
+char *replace_dollars(char *str, t_env **env_list)
+{
+	int	i;
+	int	j;
+	char *ret;
+	char *tmp;
+	char *value;
+
+	i = 0;
+	j = 0;
+	while (str[i])
+	{
+		if (str[i] == '$')
+		{
+			if (!i)
+				tmp = ft_substr(str, j + 1, i - j);
+			value = find_env_var(str, &i, env_list);
+			if (!value)
+				ret = append(ret, tmp, value);
+			else
+			{
+				ret = append(ret, tmp, value);
+				free(tmp);
+				free(value);
+			}
+			j = i;
+		}
+		while (str[i] && str[i] != '$')
+			i++;
+		tmp = ft_substr(str, j + 1, i - j);
+		ret = append(ret, tmp, value);
+	}
+	return (ret);
+}
+
+char	*cpy_str(char const *s, int start, int len, t_env **env_list)
 {
 	char	*ret;
+	char	*final;
 	int	i;
 	int	j;
 	int	count;
@@ -171,11 +269,19 @@ char	*cpy_str(char const *s, int start, int len)
 		cpy_count++;
 	}
 	ret[cpy_count] = '\0';
+	if (have_a_dollar(ret))
+	{
+		final = replace_dollars(ret, env_list);
+		if (final == NULL)
+			//Variable pas trouvee, faire qqch
+		free(ret);
+		return (final);
+	}
 	free(quotes);
 	return (ret);
 }
 
-int	copy_wrd(t_mini *shell, int *i, int nb_wrd)
+int	copy_wrd(t_mini *shell, int *i, int nb_wrd, t_env **env_list)
 {
 	int		wrd_size;
 	char	sep;
@@ -195,7 +301,7 @@ int	copy_wrd(t_mini *shell, int *i, int nb_wrd)
 			*i += 1;
 			return (-1);
 		}
-		shell->current->args[nb_wrd] = ft_substr(shell->argv, *i, (int)wrd_size);
+		shell->current->args[nb_wrd] = cpy_str(shell->argv, *i, (int)wrd_size, env_list);
 		*i += wrd_size;
 		*i += 1;
 		return (0);
@@ -203,7 +309,7 @@ int	copy_wrd(t_mini *shell, int *i, int nb_wrd)
 	else
 	{
 		wrd_size = wrd_len(shell->argv, ' ', i);
-		shell->current->args[nb_wrd] = cpy_str(shell->argv, *i, (int)wrd_size);
+		shell->current->args[nb_wrd] = cpy_str(shell->argv, *i, (int)wrd_size, env_list);
 		if (shell->current->args[nb_wrd] == NULL)
 			return (1);
 		*i += wrd_size;
@@ -214,7 +320,7 @@ int	copy_wrd(t_mini *shell, int *i, int nb_wrd)
 	return (1);
 }
 
-void	split_arg(t_mini *shell)
+void	split_arg(t_mini *shell, t_env **env_list)
 {
 	int	i;
 	int	nb_wrd;
@@ -228,9 +334,9 @@ void	split_arg(t_mini *shell)
 		while (!is_sep(shell->argv[i]) && shell->argv[i])
 		{
 			realloc_arr(shell);
-			ret = copy_wrd(shell, &i, nb_wrd);
+			ret = copy_wrd(shell, &i, nb_wrd, env_list);
 			while (ret == -1)
-				ret = copy_wrd(shell, &i, nb_wrd);
+				ret = copy_wrd(shell, &i, nb_wrd, env_list);
 			if (ret == 1)
 				return ;
 			nb_wrd++;
@@ -252,24 +358,21 @@ void	split_arg(t_mini *shell)
 
 void	parsing(t_mini *shell, t_env **env_list)
 {
-	(void)env_list;
 	if (!ft_strcmp(shell->argv, ""))
 		return ;
-	split_arg(shell);
+	split_arg(shell, env_list);
 	if (!ft_strcmp(shell->first->args[0], EXPORT))
 		export_func(env_list, shell->first->args[1]);
-	destroy_arg_lst(shell);
-	exit(0);
+	else if (!ft_strcmp(shell->first->args[0], ECHO_CMD))
+		echo_func(shell->first->args[1], NULL);
+	else if (!ft_strcmp(shell->first->args[0], EXIT))
+		exit(0);
 	/*if (!ft_strcmp(shell->arg_split[0], CD))
 		cd(shell->arg_split[1]);
-	else if (!ft_strcmp(shell->arg_split[0], ECHO_CMD))
-		echo_func(shell->arg_split[1], shell->arg_split[2]);
 	else if (!ft_strcmp(shell->argv, ENV))
 		env_func(env_list);
 	else if (!ft_strcmp(shell->arg_split[0], EXPORT))
 		export_func(env_list, shell->arg_split[1]);
-	else if (!ft_strcmp(shell->arg_split[0], EXIT))
-		exit(0);
 	else if (!ft_strcmp(shell->arg_split[0], PWD))
 		pwd();
 	else if (!ft_strcmp(shell->arg_split[0], UNSET))
