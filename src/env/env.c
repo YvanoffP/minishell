4dcaaaeba8eefd5c1de5a9f9ed9123cda8981f5f
Bima42 <tanpau42@gmail.com>
2022-02-11 17:36:41 +0100

    Export_func use now lichain

--- src/env/env.c
+++ src/env/env.c
@@ -10,6 +10,8 @@ void	env_func(t_env **env_list)
 		if (tmp->var != NULL)
 		{
 			ft_putstr_fd(tmp->var, 1);
+			write(1, "=", 1);
+			ft_putstr_fd(tmp->value, 1);
 			write(1, "\n", 1);
 		}
 		tmp = tmp->next;
