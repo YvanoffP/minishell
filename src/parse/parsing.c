d26246f02b1b60e975986a2d5b0be9d3f3c9cca0
Bima42 <tanpau42@gmail.com>
2022-02-14 16:01:13 +0100

    nodes are correctly created. Huge part of error to do. Quote too

--- src/parse/parsing.c
+++ src/parse/parsing.c
@@ -218,7 +218,7 @@ int	*parse_sep(char *str)
 	i = 0;
 	j = 0;
 	nb_sep = count_sep(str);
-	ind_sep = malloc(sizeof(int) * nb_sep);
+	ind_sep = malloc(sizeof(int) * nb_sep + 1);
 	if (!ind_sep)
 		return (NULL);
 	while (str[i])
@@ -228,6 +228,7 @@ int	*parse_sep(char *str)
 			ind_sep[j++] = i;
 		i++;
 	}
+	ind_sep[j] = 0;
 	return (ind_sep);
 }
 
@@ -304,10 +305,10 @@ void	delete_last_spaces(char **str)
 		while (i++ < len)
 			tmp[i] = (*str)[i];
 		tmp[i] = 0;
+		free(*str);
+		*str = ft_strdup(tmp);
+		free(tmp);
 	}
-	free(*str);
-	*str = ft_strdup(tmp);
-	free(tmp);
 }
 
 char	*pimp_my_string(t_mini *shell, int *sep)
@@ -344,6 +345,71 @@ char	*pimp_my_string(t_mini *shell, int *sep)
 	return (ret);
 }
 
+int	count_nb_wrd(int *sep, int *space)
+{
+	int	i;
+
+	i = 0;
+	if (*sep == 0)
+	{
+		while (*space)
+		{
+			space++;
+			i++;
+		}
+		return (i + 1);
+	}
+	else
+	{
+		while (*space < *sep)
+		{
+			i++;
+			space++;
+		}
+		return (i);
+	}
+}
+
+void	fill_array(t_mini *shell, int **sep, int **space, int *i)
+{
+	int	row;
+
+	row = 0;
+	if (**sep)
+	{
+		while (**space < **sep)
+		{
+			shell->current->args[row++] = ft_substr(shell->argv, *i, **space - *i);
+			*i = **space;
+			(*space)++;
+			skip_w_space(shell->argv, i);
+		}
+		shell->current->args[row] = 0;
+	}
+	else
+	{
+		while (**space)
+		{
+			shell->current->args[row++] = ft_substr(shell->argv, *i, **space - *i);
+			*i = **space;
+			(*space)++;
+			skip_w_space(shell->argv, i);
+		}
+		shell->current->args[row++] = ft_substr(shell->argv, *i, ft_strlen(shell->argv) - *i);
+		shell->current->args[row] = 0;
+	}
+}
+
+void	create_sep_node(t_mini *shell, int *i, int **space)
+{
+	shell->current->args = (char **)malloc(sizeof(char *) * 2);
+	shell->current->args[0] = ft_substr(shell->argv, *i, **space - *i);
+	shell->current->args[1] = 0;
+	*i = **space;
+	skip_w_space(shell->argv, i);
+	(*space)++;
+}
+
 void	alloc_args_tab(t_mini *shell, int *sep, int *space)
 {
 	int	*ptr_sep;
@@ -353,31 +419,23 @@ void	alloc_args_tab(t_mini *shell, int *sep, int *space)
 	ptr_sep = sep;
 	ptr_space = space;
 	i = 0;
-	while (*space < *sep)
+	//Boucle : shell->argv || shell->current->next 
+	while (1)
 	{
-		i++;
-		space++;
+		shell->current->args = (char **)malloc(sizeof(char *) * (count_nb_wrd(ptr_sep, ptr_space) + 1));
+		if (!shell->current->args)
+			return ; //ERROR MALLOC CATCHED
+		fill_array(shell, &ptr_sep, &ptr_space, &i);
+		if (!(*ptr_sep))
+			break ;
+		shell->current = shell->current->next;
+		if (is_sep(shell->argv[i]))
+		{
+			create_sep_node(shell, &i, &ptr_space);
+			shell->current = shell->current->next;
+		}
+		ptr_sep++;
 	}
-	shell->current->args = malloc(sizeof(char *) * (i + 1));
-	shell->current->args[i] = 0;
-	shell->current = shell->current->next;
-	sep++;
-	i = 0;
-	while (*space < *sep)
-	{
-		i++;
-		space++;
-	}
-	shell->current->args = malloc(sizeof(char *) * i);
-	shell->current->args[--i] = 0;
-	shell->current = shell->current->next;
-	sep++;
-	i = 0;
-	while (*(space++))
-		i++;
-	shell->current->args = malloc(sizeof(char *) * i + 1);
-	shell->current->args[i] = 0;
-	shell->current = shell->current->next;
 }
 
 void	split_arg(t_mini *shell, t_env **env_list)
@@ -396,7 +454,11 @@ void	split_arg(t_mini *shell, t_env **env_list)
 	init_args(shell);
 	ptr = sep;
 	while (*(ptr++))
+	{
 		create_n_add_empty_node(shell);
+		create_n_add_empty_node(shell);
+	}
+	alloc_args_tab(shell, sep, space);
 }
 
 void	parsing(t_mini *shell, t_env **env_list)
